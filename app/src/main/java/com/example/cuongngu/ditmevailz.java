2a3d8f36f1f855cf93ca27b4dc2c4d16f9f73203
Luanne <viluan922@gmail.com>
2021-10-31 19:11:12 +0700

    locc

---
+++ app/src/main/java/com/example/cuongngu/ditmevailz.java
@@ -0,0 +1,14 @@
+package com.example.cuongngu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ditmevailz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ditmevailz);
+    }
+}
